cf1b11c067972619206cf71706ca8f661d3d1b27
rodrigo93junior <schunemannjr@hotmail.com>
2021-04-13 22:13:05 -0300

    commit-130421

--- Views/CadastrarCliente.cs
+++ Views/CadastrarCliente.cs
@@ -6,72 +6,105 @@ namespace View
 {
     public class CadastrarCliente : Form
     {
+        Label lblTitulo;
+        Label lblNome;
+        TextBox nome;
+        Label lblDataNascimento;
+        TextBox dataNascimento;
+        Label lblCpf;
+        TextBox cpf;
+        Label lblDiasRetorno;
+        ComboBox diasRetorno;
+        Button btnSalvarCliente;
+        GroupBox genero;
+        RadioButton generoFeminino;
+        RadioButton generoMasculino;
         public CadastrarCliente()
         {
             this.Text = "Cadastro de Cliente";
 
-            Label lblTitulo = new Label();
+            lblTitulo = new Label();
             lblTitulo.Text = "Cadastro de Novo Cliente";
             lblTitulo.Location = new Point(180, 10);
             lblTitulo.Size = new Size(180, 40);
 
-            Label lblNome = new Label();
+            lblNome = new Label();
             lblNome.Text = "Nome Completo:";
             lblNome.Location = new Point(20, 30);
             lblNome.Size = new Size(120, 15);
 
-            TextBox Nome = new TextBox();
-            Nome.Location = new Point(20, 50);
-            Nome.Size = new Size(300, 40);
+            nome = new TextBox();
+            nome.Location = new Point(20, 50);
+            nome.Size = new Size(300, 40);
 
-            Label lblDataNascimento = new Label();
+            lblDataNascimento = new Label();
             lblDataNascimento.Text = "Data de Nascimento:";
             lblDataNascimento.Location = new Point(20, 80);
             lblDataNascimento.Size = new Size(120, 15);
 
-            TextBox DataNascimento = new TextBox();
-            DataNascimento.Location = new Point(20, 100);
-            DataNascimento.Size = new Size(300, 40);
+            dataNascimento = new TextBox();
+            dataNascimento.Location = new Point(20, 100);
+            dataNascimento.Size = new Size(300, 40);
 
-            Label lblCpf = new Label();
+            lblCpf = new Label();
             lblCpf.Text = "CPF:";
             lblCpf.Location = new Point(20, 130);
             lblCpf.Size = new Size(120, 15);
 
-            TextBox Cpf = new TextBox();
-            Cpf.Location = new Point(20, 150);
-            Cpf.Size = new Size(300, 40);
+            cpf = new TextBox();
+            cpf.Location = new Point(20, 150);
+            cpf.Size = new Size(300, 40);
 
-            Label lblDiasRetorno = new Label();
+            genero = new GroupBox();
+            genero.Text = "Genero";
+            genero.Location = new Point(20, 180);
+            genero.Size = new Size(300, 50);
+
+            generoFeminino = new RadioButton();
+            generoFeminino.Text = "Feminino";
+            generoFeminino.Location = new Point(2, 20);
+            generoFeminino.Size = new Size(100, 20);
+
+            generoMasculino = new RadioButton();
+            generoMasculino.Text = "Maculino";
+            generoMasculino.Location = new Point(120, 20);
+            generoMasculino.Size = new Size(100, 20);
+
+            genero.Controls.Add(generoFeminino);
+            genero.Controls.Add(generoMasculino);
+
+            lblDiasRetorno = new Label();
             lblDiasRetorno.Text = "Dias para Retorno:";
-            lblDiasRetorno.Location = new Point(20, 180);
+            lblDiasRetorno.Location = new Point(20, 240);
             lblDiasRetorno.Size = new Size(120, 15);
 
-            TextBox DiasRetorno = new TextBox();
-            DiasRetorno.Location = new Point(20, 200);
-            DiasRetorno.Size = new Size(300, 40);
+            diasRetorno = new ComboBox();
+            diasRetorno.Location = new Point(20, 260);
+            diasRetorno.Size = new Size(300, 40);
+            diasRetorno.Items.AddRange(new String[] { "1 dia", "5 dias", "10 dias", "15 dias" });
 
-            Button btSalvarCliente = new Button();
-            btSalvarCliente.Text = "Salvar";
-            btSalvarCliente.Location = new Point(150, 250);
-            btSalvarCliente.Size = new Size(200, 40);
-            btSalvarCliente.Click += new EventHandler(this.btSalvarCliente);
+            btnSalvarCliente = new Button();
+            btnSalvarCliente.Text = "Salvar";
+            btnSalvarCliente.Location = new Point(120, 300);
+            btnSalvarCliente.Size = new Size(200, 40);
+            btnSalvarCliente.Click += new EventHandler(this.botaoSalvarCliente);
 
 
             this.Size = new Size(540, 400);
             this.Controls.Add(lblTitulo);
             this.Controls.Add(lblNome);
-            this.Controls.Add(Nome);
+            this.Controls.Add(nome);
             this.Controls.Add(lblDataNascimento);
-            this.Controls.Add(DataNascimento);
-            this.Controls.Add(Cpf);
+            this.Controls.Add(dataNascimento);
+            this.Controls.Add(cpf);
             this.Controls.Add(lblCpf);
+            this.Controls.Add(genero);
             this.Controls.Add(lblDiasRetorno);
-            this.Controls.Add(DiasRetorno);
-            this.Controls.Add(btSalvarCliente);
+            this.Controls.Add(diasRetorno);
+            this.Controls.Add(btnSalvarCliente);
         }
 
-        private void btSalvarCliente(object sender, EventArgs e)
+        private void botaoSalvarCliente(object sender, EventArgs e)
         {
             this.Close();
         }
